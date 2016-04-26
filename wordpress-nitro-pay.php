c01533629a28bb0e8d203d19d5640ad493672db3
Ilton Silveira <ton.garcia.jr@gmail.com>
2016-04-26 13:33:40 -0300

    base PlugIn

---
+++ wordpress-nitro-pay.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+     * Plugin Name: NitroPay
+     * Plugin URI: https://github.com/NITRO-START/wordpress_nitro_pay
+     * Description: NitroPay PlugIn conector do meio de pagamento NitroPay da NitroStart
+     * Author: NITRO START
+     * Version: 1.0
+     * Author URI: http://www.nitrostart.me
+    */
+?>
